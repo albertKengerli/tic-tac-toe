6b2e8d004a0fb05ef1a2e36fffaab4a6a5c22522
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-14 12:40:25 +0300

    feat: implement getCurrentPlayerSymbol, nextTurn, getFieldValue methods

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,17 @@
 class TicTacToe {
     constructor() {
-
+      this.currentPlayer = "x";
+      this.field = Array(3).fill(null).map(e => e = Array(3).fill(null));
     }
 
     getCurrentPlayerSymbol() {
-
+      return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+      if (this.field[columnIndex][rowIndex] != null) return;
+      this.field[columnIndex][rowIndex] = this.currentPlayer;
+      this.currentPlayer = (this.currentPlayer === "x") ? this.currentPlayer = "o" : this.currentPlayer = "x"; 
     }
 
     isFinished() {
@@ -28,7 +31,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+      return this.field[colIndex][rowIndex];
     }
 }
 
